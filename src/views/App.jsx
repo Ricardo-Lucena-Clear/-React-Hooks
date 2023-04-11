493cba72530c350200de3ae27aa6546039341d04
Ricardo-Lucena-Clear <126709276+Ricardo-Lucena-Clear@users.noreply.github.com>
2023-04-11 11:31:09 -0300

    useContext #01

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,19 +1,25 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router } from  'react-router-dom'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import DataContext,{ data } from '../data/DataContext' 
 
 const App = props => {
-
+    const [state, setState] = useState(data)
+/*  value= {{state, setState}}  (duas chaves no mesmo lugar servem para substituir o uso de (:) exemplos:
+state:state, setState:setState */
     return (
-                <div className="App">
+        <DataContext.Provider value= {{state, setState}}> 
+       <div className="App">
                     <Router>
                         <Menu />
                         <Content />
                     </Router>
                 </div>
+        </DataContext.Provider>
+                
     )
 }
 
